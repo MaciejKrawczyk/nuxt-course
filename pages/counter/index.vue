f207f26410cc75b7e24576d9fd218e2cde8a2819
Maciej Krawczyk <kontakt.maciejkrawczyk@gmail.com>
2023-12-06 11:41:25 +0100

    state management

---
+++ pages/counter/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Counter: {{ counter }}
+    <button @click="counter++">
+      +
+    </button>
+    <button @click="counter--">
+      -
+    </button>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+import {useCounter} from "~/composables/states";
+
+const counter = useCounter()
+
+</script>
